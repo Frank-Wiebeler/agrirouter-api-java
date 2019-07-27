4336534e38a9a0ee6acb0e426f3a0e016fe58143
Frank <frank.wiebeler@dev4agriculture.de>
2019-07-27 13:31:54 +0200

    -FIX: API had mixed revisions

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/onboard/RevokingService.java
+++
@@ -1,9 +0,0 @@
-package com.dke.data.agrirouter.api.service.onboard;
-
-import com.dke.data.agrirouter.api.enums.RevokeResponse;
-import com.dke.data.agrirouter.api.service.parameters.RevokeParameters;
-
-public interface RevokingService {
-
-  RevokeResponse revoke(RevokeParameters revokeParameters);
-}
